8370ebdd286c93a9b57ad55b79b0053f109916a3
Pearson Mewbourne <pmewbourne@hmc.edu>
2024-05-27 15:55:53 -0700

    still bug fixing

--- crawlerDemo/crawlerDemo/spiders/builder_spider.py
+++ crawlerDemo/crawlerDemo/spiders/builder_spider.py
@@ -18,9 +18,14 @@ class crawlingSpiderDemo(CrawlSpider):
     )
     
     def parse_item(self, response):
+        item_name = response.css(".product-details h2::text").extract()
+        item_num = response.css(".product-id span[itemprop='sku']::text").extract_first()
+        price = response.css("span[id='price']::text").extract_first()
+        stock = response.css("span[id='availability']::text").extract_first()
+
         yield {
-            "item name":response.css(".product-details h2::text").extract(),
-            "item number": response.css(".product-id span[itemprop='sku']::text").extract_first().replace(":","").strip(),
-            "price":response.css("span[id='price']::text").extract_first().replace("$",""),
-            "stock":response.css("span[id='availability']::text").extract_first().replace(" In stock","")
+            "item name": item_name if item_name else "N/A",
+            "item number": item_num.replace(":","").strip() if item_num else "N/A",
+            "price": price.replace("$","") if price else "N/A",
+            "stock": stock.replace(" In stock","") if stock else "N/A"
             }
