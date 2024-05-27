a710fbd5ac398684e37f941ff17e03ef073ca466
Pearson Mewbourne <pmewbourne@hmc.edu>
2024-05-27 16:44:54 -0700

    can't figure it out, but I gotta turn this in

--- crawlerDemo/crawlerDemo/spiders/builder_spider.py
+++ crawlerDemo/crawlerDemo/spiders/builder_spider.py
@@ -12,6 +12,7 @@ class crawlingSpiderDemo(CrawlSpider):
                        "DUPEFILTER_CLASS": 'scrapy.dupefilters.RFPDupeFilter',
                        }
 
+
     rules = (
         Rule(LinkExtractor(deny = ["/checkout.asp","/add_cart.asp", "/view_cart.asp", "/error.asp",
                            "/shipquote.asp", "/rssfeed.asp", "/mobile/", "/cgi-bin/",
@@ -20,13 +21,13 @@ class crawlingSpiderDemo(CrawlSpider):
     )
     
     def parse_item(self, response):
-        item_name = response.css(".product-details h2::text").extract()
+        item_name = response.css(".product-details h2::text").extract_first()
         item_num = response.css(".product-id span[itemprop='sku']::text").extract_first()
         price = response.css("span[id='price']::text").extract_first()
         stock = response.css("span[id='availability']::text").extract_first()
         if item_name:
             yield {
-                "item name": item_name ,
+                "item name": item_name,
                 "item number": item_num.replace(":","").strip(),
                 "price": price.replace("$",""),
                 "stock": stock.replace(" In stock","")
