1a2b720c61e4ac8e47fa39d2dcba6220c83a0f65
Pearson Mewbourne <pmewbourne@hmc.edu>
2024-05-30 12:56:07 -0700

    commented

--- crawlerDemo/crawlerDemo/spiders/stoneworld_spider.py
+++ crawlerDemo/crawlerDemo/spiders/stoneworld_spider.py
@@ -1,12 +1,14 @@
-# Only slightly changed from before. Wanted practice doing it on a new website. 
-# Other comments are copied from crawling_spider.py
 
-# In order to run this, navigate to webCrawlerDemo/crawlerDemo via the command line, then execute "scrapy crawl chessCrawler"
+# In order to run this, navigate to webCrawlerDemo/crawlerDemo via the command line, then execute "scrapy crawl stoneworldCrawler -a filename=stoneworldCrawl.txt"
+# -a sets a variable in the execute to be taken from the commandline
+# filename= is setting that variable 
+# stoneworldCrawl.txt is full of info for the crawler.
 
 # CrawlSpider is for the crawler, Rule is for the behavior, linkExtractor is for accessing the domains.
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+# Used to clean up strings
 import re
 
 
@@ -14,8 +16,11 @@ class crawlingSpiderDemo(CrawlSpider):
 
     # fields:
     # name - How crawler is referred to in terminal
-    # allowed_domains - controls the scope of the urls that the crawler will attempt to access
-    # start_urls - where the crawler will start searching from
+    
+    # crawl_domains will be set to domains to crawl
+    # info_domains will be set to domains to scrape
+    # cssDictList is a list of strings used for populating the csv
+    # cssDictListLen will be set to the len of said list
     name = "stoneworldCrawler"
     crawl_domains = ""
     info_domains = ""
@@ -23,7 +28,7 @@ class crawlingSpiderDemo(CrawlSpider):
     cssDictListLen = 0
 
 
-    custom_settings = {"ROBOTSTXT_OBEY":True,
+    custom_settings = {"ROBOTSTXT_OBEY":True, # Follow the robots.txt of the website
                        "FEED_URI": "stoneWorld_%(time)s.csv", # Output results to this file name
                        "FEED_FORMAT": "csv", # File type of output
                        "DUPEFILTER_CLASS": 'scrapy.dupefilters.RFPDupeFilter', # Supposed to stop crawler from revisiting same url multiple times
@@ -35,17 +40,16 @@ class crawlingSpiderDemo(CrawlSpider):
         super(crawlingSpiderDemo, self).__init__(*args, **kwargs)
         if filename:
             with open(filename, 'r') as f:
+                # Take in the info from the .txt file and use it to set up the spider
                 fileArgs = f.readlines()
                 self.start_urls = [fileArgs[0].strip()]
                 self.allowed_domains = [fileArgs[1].strip()]
                 self.crawl_domains = [fileArgs[2].strip()]
                 self.info_domains = [fileArgs[3].strip()]
-                cssFirstSlice = int(fileArgs[4])
-                cssLastSlice = int(fileArgs[5])
-                self.cssDictList = fileArgs[cssFirstSlice:cssLastSlice]
+                self.cssDictList = fileArgs[4:]
                 self.cssDictListLen = len(self.cssDictList)
                 # Adapted from https://stackoverflow.com/questions/27509489/how-to-dynamically-set-scrapy-rules
-                crawlingSpiderDemo.rules = (
+                crawlingSpiderDemo.rules = ( # Dynamic ules need to be set by editing the initializer b/c they're formed at compile time
                                             Rule (LinkExtractor(allow=(self.info_domains)), callback="parse_item",  follow= True),
                                             Rule (LinkExtractor(allow=(self.crawl_domains)),  follow= True),
                                             )
@@ -54,16 +58,21 @@ class crawlingSpiderDemo(CrawlSpider):
                 
 
     
-    
+    # Function to clean up the strings from webpages
     def stringCleaner(self, strToFix):
-        if strToFix:
+        """
+        input: a string obtained through the css of a webpage
+        output: a string without extraneous elements, like newline characters or redundant prefixes
+        """
+        if strToFix: # Chance the page lacks a specific element, maybe like fax number. Check first.
             cleanStr = strToFix.replace("\n","").replace("mailto:","").replace("Fax: ","").replace("Phone: ","").replace('"','').strip()
+            # Adapted from https://www.codecademy.com/resources/docs/python/regex/sub
             cleanStr = re.sub(r'\s+',' ', cleanStr)
             return cleanStr
         else:
             return "Not Provided"
 
-    # Function to parse the current page and get the info of a chess opening
+    # Function to parse the current page and get the info set by the input file
     def parse_item(self, response):
         """
         input: response, the current page being scraped
@@ -71,6 +80,7 @@ class crawlingSpiderDemo(CrawlSpider):
         """
         returnDict = {}
         for i in range (0, self.cssDictListLen//2):
+            # In input file, each elem of dictionary is a pair of lines, the key followed by value.
             curIndex = 2*i
             key = self.cssDictList[curIndex].strip()
             value = self.stringCleaner(response.css(self.cssDictList[curIndex + 1]).get())
