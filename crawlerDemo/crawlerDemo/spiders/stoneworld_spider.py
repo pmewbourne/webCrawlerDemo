4393bdaabc3d99c3754a9f5fe64c1d9c267bd065
Pearson Mewbourne <pmewbourne@hmc.edu>
2024-05-30 12:43:41 -0700

    confirmed, totally works. onto commenting

--- crawlerDemo/crawlerDemo/spiders/stoneworld_spider.py
+++ crawlerDemo/crawlerDemo/spiders/stoneworld_spider.py
@@ -22,17 +22,6 @@ class crawlingSpiderDemo(CrawlSpider):
     cssDictList = []
     cssDictListLen = 0
 
-    # css_companyName = ""
-    # css_companyAddress = ""
-    # css_companyCity = ""
-    # css_companyCountry = ""
-    # css_companyPhone = ""
-    # css_companyFax = ""
-    # css_companyURL = ""
-    # css_companyEmail = ""
-    # css_contactName = ""
-    # css_contactPhone = ""
-    # css_contactEmail = ""
 
     custom_settings = {"ROBOTSTXT_OBEY":True,
                        "FEED_URI": "stoneWorld_%(time)s.csv", # Output results to this file name
@@ -55,17 +44,6 @@ class crawlingSpiderDemo(CrawlSpider):
                 cssLastSlice = int(fileArgs[5])
                 self.cssDictList = fileArgs[cssFirstSlice:cssLastSlice]
                 self.cssDictListLen = len(self.cssDictList)
-                # self.css_companyName = fileArgs[4].strip()
-                # self.css_companyAddress = fileArgs[5].strip()
-                # self.css_companyCity = fileArgs[6].strip()
-                # self.css_companyCountry = fileArgs[7].strip()
-                # self.css_companyPhone = fileArgs[8].strip()
-                # self.css_companyFax = fileArgs[9].strip()
-                # self.css_companyURL = fileArgs[10].strip()
-                # self.css_companyEmail = fileArgs[11].strip()
-                # self.css_contactName = fileArgs[12].strip()
-                # self.css_contactPhone = fileArgs[13].strip()
-                # self.css_contactEmail = fileArgs[14].strip()
                 # Adapted from https://stackoverflow.com/questions/27509489/how-to-dynamically-set-scrapy-rules
                 crawlingSpiderDemo.rules = (
                                             Rule (LinkExtractor(allow=(self.info_domains)), callback="parse_item",  follow= True),
@@ -75,9 +53,6 @@ class crawlingSpiderDemo(CrawlSpider):
 
                 
 
-    # rules = (
-    #     Rule(LinkExtractor(allow = rule_domains), callback = "parse_item"),
-    # )
     
     
     def stringCleaner(self, strToFix):
@@ -94,17 +69,6 @@ class crawlingSpiderDemo(CrawlSpider):
         input: response, the current page being scraped
         output: returns a dictionary containing the name of a chess opening and what broad section it falls under.
         """
-        # companyName = response.css(self.css_companyName).get()
-        # companyAddress = response.css(self.css_companyAddress).get()
-        # companyCity = response.css(self.css_companyCity).get()
-        # companyCountry = response.css(self.css_companyCountry).get()
-        # companyPhone = response.css(self.css_companyPhone).get()
-        # companyFax = response.css(self.css_companyFax).get()
-        # companyURL = response.css(self.css_companyURL).get()
-        # companyEmail = response.css(self.css_companyEmail).get()
-        # contactName = response.css(self.css_contactName).get()
-        # contactPhone = response.css(self.css_contactPhone).get()
-        # contactEmail = response.css(self.css_contactEmail).get()
         returnDict = {}
         for i in range (0, self.cssDictListLen//2):
             curIndex = 2*i
@@ -112,18 +76,3 @@ class crawlingSpiderDemo(CrawlSpider):
             value = self.stringCleaner(response.css(self.cssDictList[curIndex + 1]).get())
             returnDict[key] = value
         yield returnDict
-        # yield {
-            
-            # Have to use css style to access these page elements
-            # "Name":companyName.strip() if companyName else "Not Provided",
-            # "Address":companyAddress.strip() if companyAddress else "Not Provided",
-            # "City":companyCity.strip() if companyCity else "Not Provided",
-            # "Country":companyCountry.strip() if companyCountry else "Not Provided",
-            # "Phone Number":companyPhone.strip() if companyPhone else "Not Provided",
-            # "Fax Number":companyFax.strip() if companyFax else "Not Provided",
-            # "URL":companyURL.strip() if companyURL else "Not Provided",
-            # "Email":companyEmail.replace("mailto:","").strip() if companyEmail else "Not Provided",
-            # "Contact Name":contactName.strip() if contactName else "Not Provided",
-            # "Contact Phone Number":contactPhone.strip() if contactPhone else "Not Provided",
-            # "Contact Email":contactEmail.replace("mailto:","").strip() if contactEmail else "Not Provided"
-        # }
